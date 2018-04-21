22ca3a0e5a5df84d061844a2cdeeb265b5eb6f16
Jose Garcia <garciajd90@gmail.com>
2018-04-21 01:55:49 -0700

    get around automation detection

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const jsonfile = require('jsonfile');
 
+
+const sleep = (time) => new Promise((resolve) => {
+  setTimeout(resolve, time);
+});
+
+
 const getEventsFromHtml = ($) => {
   console.log('Parsing HTML data...');
   const rows = $('tbody tr');
@@ -41,38 +47,12 @@ const getEventsFromHtml = ($) => {
     .filter(({ type }) => type !== 'Pokémon League')
     .forEach((event) => {
       eventsPromise = eventsPromise
-        .then(() => {
-          console.log(`Fetching data for event: ${event.name}`);
-          const options = {
-            uri: `https://www.pokemon.com${event.relativeLink}`,
-            transform: cheerio.load
-          };
-
-          return request(options);
-        })
-        .then(($) => {
-          console.log(`Parsing HTML data for event: ${event.name}`);
-          const forms = $('.whiteBucket form');
-
-          const section1Rows = $(forms[0]).find('ol li');
-          const tournamentID = $(section1Rows[1]).contents()[1].data;
-
-          const locationInfoRows = $(forms[2]).find('ol li');
-
-          const location = {
-            name: $(locationInfoRows[0]).contents()[1].data,
-            address: $(locationInfoRows[1]).contents()[1].data,
-            city: $(locationInfoRows[3]).contents()[1].data,
-            mapLink: encodeURI($(locationInfoRows[7]).find('a').attr('href'))
-          };
-
-          event.tournamentID = tournamentID;
-          event.location = location;
-        })
-        .catch(() => console.error(`An error occurred while attempting to obtain data for event: ${event.name}`));
+        .then(() => getLocationInfoForEvent(event));
     });
 
   eventsPromise
+    .then(() => console.log('Successfully obtained all data.'))
+    .catch(() => console.log('Failed to obtain some data.'))
     .then(() => {
       console.log('Writing events.json...');
       jsonfile.writeFileSync('events.json', events, { spaces: 2 });
@@ -80,14 +60,56 @@ const getEventsFromHtml = ($) => {
     });
 };
 
-const url = 'https://www.pokemon.com/us/play-pokemon/pokemon-events/find-an-event/?country=176&postal_code=&city=Phoenix&event_name=&location_name=&address=&state_object=&state_other=&distance_within=25&start_date=0&end_date=30&event_type=league&event_type=tournament&event_type=premier&product_type=tcg&sort_order=when&results_pp=50';
 
-const options = {
-  uri: url,
-  transform: cheerio.load
-};
+const getLocationInfoForEvent = (event, reattemptDelaySeconds = 10) => {
+  console.log(`Fetching data for event: ${event.name}`);
+
+  return request({
+    uri: `https://www.pokemon.com${event.relativeLink}`,
+    transform: cheerio.load
+  })
+    .then(($) => {
+      console.log(`Parsing HTML data for event: ${event.name}`);
+      const forms = $('.whiteBucket form');
+
+      const section1Rows = $(forms[0]).find('ol li');
+      const tournamentID = $(section1Rows[1]).contents()[1].data;
+
+      const locationInfoRows = $(forms[2]).find('ol li');
+
+      const location = {
+        name: $(locationInfoRows[0]).contents()[1].data,
+        address: $(locationInfoRows[1]).contents()[1].data,
+        city: $(locationInfoRows[3]).contents()[1].data,
+        mapLink: encodeURI($(locationInfoRows[7]).find('a').attr('href'))
+      };
+
+      event.tournamentID = tournamentID;
+      event.location = location;
+
+      // return sleep(1000);
+    })
+    .catch((e) => {
+      if (e.statusCode !== 503) {
+        console.error(e);
+        return Promise.reject();
+      }
+
+      if (reattemptDelaySeconds > 60) {
+        console.log('Giving up.');
+        return Promise.reject();
+      }
+
+      console.log(`We got caught! Will re-attempt in ${reattemptDelaySeconds} seconds...`);
+      return sleep(1000 * reattemptDelaySeconds)
+        .then(() => getLocationInfoForEvent(event, reattemptDelaySeconds + 10));
+    });
+}
+
 
 console.log('Fetching events data...');
-request(options)
+request({
+  uri: 'https://www.pokemon.com/us/play-pokemon/pokemon-events/find-an-event/?country=176&postal_code=&city=Phoenix&event_name=&location_name=&address=&state_object=&state_other=&distance_within=25&start_date=0&end_date=30&event_type=league&event_type=tournament&event_type=premier&product_type=tcg&sort_order=when&results_pp=50',
+  transform: cheerio.load
+})
   .then(getEventsFromHtml);
-  
